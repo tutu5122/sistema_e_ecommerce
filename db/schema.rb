cc266fb3e1f3f235cc0fe17a2b07cef673316cc9
Cristobal Dominguez <cristobald@gmail.com>
2022-07-13 18:56:14 -0400

    Agrega Scaffold de Product

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_07_13_224044) do
+ActiveRecord::Schema.define(version: 2022_07_13_225419) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "categories", force: :cascade do |t|
     t.string "name", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "products", force: :cascade do |t|
+    t.string "name", null: false
+    t.text "description"
+    t.integer "stock", default: 0
+    t.decimal "price"
+    t.string "sku"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
