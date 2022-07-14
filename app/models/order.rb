14020904e87535f58766a54bf9d1a277689ebcda
Cristobal Dominguez <cristobald@gmail.com>
2022-07-14 19:41:40 -0400

    Agrega funcionalidad de agregar numero a la orden

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,3 +1,26 @@
 class Order < ApplicationRecord
   belongs_to :user
+
+  enum state: %i[]
+
+  before_create -> { generate_number(hash_prefix, hash_size) }
+
+  def generate_number(prefix, size)
+    self.number ||= loop do
+      random = random_candidate(prefix, size)
+      break random unless self.class.exists?(number: random)
+    end
+  end
+
+  def random_candidate(prefix, size)
+    "#{prefix}#{Array.new(size){ rand(size) }.join }"
+  end
+
+  def hash_prefix
+    'BO'
+  end
+
+  def hash_size
+    9
+  end
 end
