187a308eeed32b4f21d793c0646585255527eacf
Cristobal Dominguez <cristobald@gmail.com>
2022-07-13 21:53:15 -0400

    Agrega Otras configuraciones de Devise y Home Controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,12 @@
 Rails.application.routes.draw do
-
+  
+  get 'home/index'
   devise_for :admins
 
   authenticate :admin do
     resources :products
     resources :categories
   end
-  
+
   root 'home#index'
 end
